e442c40e809341e6849e7e328031ae4d1d5f0d3b
Suzi-Clark <susannaclark95@gmail.com>
2023-03-17 17:37:19 +0000

    added lots to tests, currently trying to figure out why game board instructions test isn't working

--- noughts-crosses/src/components/GameBoard.tsx
+++ noughts-crosses/src/components/GameBoard.tsx
@@ -15,8 +15,8 @@ export default function GameBoard({handleClick, noughtsNext, grid}: gameBoardPro
     
 
     return (<div>
-        <h2>Gameboard here.</h2>
-        <div className='game-board'>
+            <p data-testid='instructions'>Take it in turns.<br/>0s go first.<br/>Get 3 in a vertical, horizontal or diagonal line to win.<br/><strong>Click a square to play!</strong></p>
+        <div data-testid='game-board' className='game-board'>
             {grid.map((item, index) =>
                 <GameButton key={index.toString()} text={item}/>
             )}
--- noughts-crosses/tests/GameBoard.test.tsx
+++ noughts-crosses/tests/GameBoard.test.tsx
@@ -1,8 +1,49 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import '@testing-library/jest-dom'
 import GameBoard from "@/components/GameBoard";
+import { gridTuple } from '@/types';
 
-test("homepage does not render EndGame message on load", () => {
-  //render GameBoard here - need to work out how to mock props
+describe('GameBoard', () => {
+  const handleClick = jest.fn();
+  const noughtsNext = true;
+  const grid: gridTuple = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
+
+  test('renders game board div', () => {
+    render(<GameBoard handleClick={handleClick} noughtsNext={noughtsNext} grid={grid} />);
+    const gameBoard = screen.getByTestId('game-board');
+    expect(gameBoard).toBeInTheDocument();
+  });
+
+  test('renders game board div with 9 buttons in it', () => {
+    render(<GameBoard handleClick={handleClick} noughtsNext={noughtsNext} grid={grid} />);
+    const buttons = screen.getAllByTestId('game-button');
+    expect(buttons.length).toBe(9);
+  });
+
+  test('renders instructions on the page', () => {
+    const info = screen.getByTestId('instructions')
+    expect(info).toBeInTheDocument();
+  })
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// test("renders all 9 noughts and crosses buttons", () => {
+//   //render GameBoard here - need to work out how to mock props
+//   render(<GameBoard handleClick={() => console.log('hi')} noughtsNext={true} grid={[' ', ' ', ' ', ' ', ' ', ' ',' ', ' ', ' ']}/>);
+//   const gameButtons = screen.getByRole('button');
+
+// });
