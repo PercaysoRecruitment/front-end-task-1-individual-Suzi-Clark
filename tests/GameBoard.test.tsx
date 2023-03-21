c3fac9b288ffa764b87085c0e752ab8b789da5b4
Suzi-Clark <susannaclark95@gmail.com>
2023-03-21 11:08:15 +0000

    updated gameboard test

--- tests/GameBoard.test.tsx
+++ tests/GameBoard.test.tsx
@@ -10,19 +10,19 @@ describe('GameBoard', () => {
   const grid: gridTuple = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
 
   test('renders game board div', () => {
-    render(<GameBoard handleClick={handleClick} noughtsNext={noughtsNext} grid={grid} />);
+    render(<GameBoard handleClick={handleClick} grid={grid} />);
     const gameBoard = screen.getByTestId('game-board');
     expect(gameBoard).toBeInTheDocument();
   });
 
   test('renders game board div with 9 buttons in it', () => {
-    render(<GameBoard handleClick={handleClick} noughtsNext={noughtsNext} grid={grid} />);
+    render(<GameBoard handleClick={handleClick} grid={grid} />);
     const buttons = screen.getAllByTestId('game-button');
     expect(buttons.length).toBe(9);
   });
 
   test('renders instructions on the page', () => {
-    render(<GameBoard handleClick={handleClick} noughtsNext={noughtsNext} grid={grid} />);
+    render(<GameBoard handleClick={handleClick} grid={grid} />);
     const info = screen.getByTestId('instructions')
     expect(info).toBeInTheDocument();
   })
